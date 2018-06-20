d3d7f5c8396ad9ec25c8a20f3f7d265cba27ddf2
Allen Marshall <allen.marshall.22@gmail.com>
2018-06-19 22:10:34 -0500

    Main window GUI now shows undo stack state a bit more, e.g. showing the name of the command to undo

--- src/scriptaseq/internal/gui/qt_ui_types/main_window.py
+++ src/scriptaseq/internal/gui/qt_ui_types/main_window.py
@@ -58,19 +58,24 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     can_redo -- Boolean indicating whether we can currently redo.
     """
     self.actionRedo.setEnabled(can_redo)
+    # TODO: Don't hard-code the command text.
+    self.actionRedo.setText('&Redo {}'.format(self._undo_stack.redoText()) if can_redo else '&Redo')
   
   def _set_can_undo(self, can_undo):
     """Called when the status of whether we have an action to undo changes.
     can_undo -- Boolean indicating whether we can currently undo.
     """
     self.actionUndo.setEnabled(can_undo)
+    # TODO: Don't hard-code the command text.
+    self.actionUndo.setText('&Undo {}'.format(self._undo_stack.undoText()) if can_undo else '&Undo')
   
   def _set_undo_stack_clean(self, is_clean):
     """Called when the undo stack changes clean status.
     is_clean -- Boolean indicating whether the undo stack is currently in a clean state.
     """
-    # TODO: Probably add an asterisk to window title when not clean.
-    pass
+    self.actionSaveProject.setEnabled(not is_clean)
+    # TODO: Don't hard-code the title, and probably include project filename when possible.
+    self.setWindowTitle('ScriptASeq' if is_clean else 'ScriptASeq*')
   
   def _push_undo_command(self, command):
     """Pushes an undo command onto the undo stack
