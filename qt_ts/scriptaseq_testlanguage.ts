4939f6543c1ed8589ec399d139a36ae4566a8df1
Allen Marshall <allen.marshall.22@gmail.com>
2018-08-31 15:36:19 -0500

    Added some basic infrastructure to support localization

---
+++ qt_ts/scriptaseq_testlanguage.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/scriptaseq/internal/gui/qt_ui_types/main_window.py" line="16"/>
+        <source>ScriptASeq</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="112"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="113"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="114"/>
+        <source>Node Tree</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="115"/>
+        <source>Node Properties</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="116"/>
+        <source>Property Value Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="117"/>
+        <source>&amp;Open...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="118"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="119"/>
+        <source>&amp;New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="120"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="121"/>
+        <source>&amp;Undo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="122"/>
+        <source>&amp;Redo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/scriptaseq/internal/generated/qt_ui/main_window.py" line="123"/>
+        <source>Save &amp;As...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- src/scriptaseq/main.py
+++ src/scriptaseq/main.py
@@ -1,15 +1,39 @@
 """ScriptASeq launcher script"""
 
+from PyQt5.Qt import QTranslator, QLocale, QLibraryInfo, QDir
 from PyQt5.QtWidgets import QApplication
 import sys
 
-from scriptaseq.internal.gui.qt_ui_types.main_window import MainWindow
 from scriptaseq.internal.gui.qml_types.registration import register_qml_types
+from scriptaseq.internal.gui.qt_ui_types.main_window import MainWindow
 
 
 if __name__ == '__main__':
   qt_app = QApplication(sys.argv)
+  
+  # Set up translation.
+  
+  qt_translator = QTranslator(qt_app)
+  if not qt_translator.load('qt_' + QLocale.system().name(), QLibraryInfo.location(QLibraryInfo.TranslationsPath)):
+    # TODO: Log the failure to load translation information.
+    pass
+  elif not qt_app.installTranslator(qt_translator):
+    # TODO: Log the failure to install translation information.
+    pass
+  
+  app_translator = QTranslator(qt_app)
+  app_translations_path = QDir.toNativeSeparators('./qt_qm/translations')
+  if not app_translator.load('scriptaseq_' + QLocale.system().name(), app_translations_path):
+    # TODO: Log the failure to load translation information.
+    pass
+  elif not qt_app.installTranslator(app_translator):
+    # TODO: Log the failure to load translation information.
+    pass
+  
   register_qml_types()
+  
+  # Construct the main window.
   main_window = MainWindow()
   main_window.showMaximized()
+  
   sys.exit(qt_app.exec_())
