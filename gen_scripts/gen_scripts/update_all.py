5859ac3a6185adf138871a29e9b83101ac4e30e8
Allen Marshall <allen.marshall.22@gmail.com>
2018-09-01 04:36:29 -0500

    Added option in update_all.py script to skip updating the translation source files

--- gen_scripts/gen_scripts/update_all.py
+++ gen_scripts/gen_scripts/update_all.py
@@ -2,11 +2,18 @@
 Should be executed from the project's main directory."""
 
 import runpy
+import argparse
 
 
 if __name__ == '__main__':
   
+  arg_parser = argparse.ArgumentParser()
+  arg_parser.add_argument('--no-update-translation-source-files', dest='no_update_translation_source_files',
+    action='store_const', const=True, default=False)
+  args = arg_parser.parse_args()
+  
   runpy.run_module('gen_scripts.qt_ui_to_python', run_name='__main__')
-  runpy.run_module('gen_scripts.update_translation_source_files', run_name='__main__')
+  if not args.no_update_translation_source_files:
+    runpy.run_module('gen_scripts.update_translation_source_files', run_name='__main__')
   runpy.run_module('gen_scripts.update_translation_release_files', run_name='__main__')
   runpy.run_module('gen_scripts.package_qt_resources', run_name='__main__')
